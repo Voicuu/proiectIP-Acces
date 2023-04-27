f421880352b200ed7ab81b6044d7cb57396996e8
Voicuu <andrei.voicu2@student.upt.ro>
2023-04-27 19:38:54 +0300

    push

--- PcMod/proiectIP/Login.cs
+++ PcMod/proiectIP/Login.cs
@@ -15,6 +15,7 @@ namespace proiectIP
         public Login()
         {
             InitializeComponent();
+            
         }
 
         private void button1_Click(object sender, EventArgs e)
