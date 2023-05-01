bdc9398018363c556aafd138a59e5c9855e2f5f2
unknown <ytbcata@gmail.com>
2023-05-01 15:30:52 +0300

    Initial Project with the LogIn/First Screen Added, but with 0 functionality

---
+++ AcessApp/src/components/CustomInput/CustomInput.js
@@ -0,0 +1,32 @@
+import { View, Text, TextInput, StyleSheet } from 'react-native';
+import IMEI from 'react-native-imei';
+import React from 'react';
+
+const CustomInput = () => {
+  return (
+    <View style={styles.container}>
+      <TextInput
+        placeholder="aici o sa vina codul IMEI"
+        style={styles.input}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    width: '100%',
+    justifyContent: 'center',
+    borderColor: '#e8e8e8',
+    borderWidth: 1,
+    borderRadius: 5,
+  },
+  input: {
+    width: '100%',
+    textAlign: 'center',
+    fontFamily: 'Poppins-Regular', 
+  },
+});
+
+export default CustomInput;
