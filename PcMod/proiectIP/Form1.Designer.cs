7f5a1fdeed127a27b3895cd7e289c37833f8c0db
Voicuu <andrei.voicu2@student.upt.ro>
2023-05-02 20:49:32 +0300

    added functionality for gate state

--- PcMod/proiectIP/Form1.Designer.cs
+++ PcMod/proiectIP/Form1.Designer.cs
@@ -35,8 +35,8 @@
             this.oraCurenta = new System.Windows.Forms.Label();
             this.timerOraCurenta = new System.Windows.Forms.Timer(this.components);
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.label6 = new System.Windows.Forms.Label();
+            this.pictureBoxGate = new System.Windows.Forms.PictureBox();
+            this.labelGateState = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.label9 = new System.Windows.Forms.Label();
@@ -52,7 +52,7 @@
             this.btnDisconnectPortar = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pbBackground)).BeginInit();
             this.groupBox2.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGate)).BeginInit();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
@@ -63,9 +63,10 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.pbBackground.Image = ((System.Drawing.Image)(resources.GetObject("pbBackground.Image")));
-            this.pbBackground.Location = new System.Drawing.Point(-10, -4);
+            this.pbBackground.Location = new System.Drawing.Point(-13, -5);
+            this.pbBackground.Margin = new System.Windows.Forms.Padding(4);
             this.pbBackground.Name = "pbBackground";
-            this.pbBackground.Size = new System.Drawing.Size(782, 440);
+            this.pbBackground.Size = new System.Drawing.Size(1043, 542);
             this.pbBackground.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pbBackground.TabIndex = 0;
             this.pbBackground.TabStop = false;
@@ -75,7 +76,8 @@
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(224, 9);
+            this.label1.Location = new System.Drawing.Point(299, 11);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(322, 42);
             this.label1.TabIndex = 1;
@@ -87,7 +89,8 @@
             this.oraCurenta.AutoSize = true;
             this.oraCurenta.BackColor = System.Drawing.Color.Transparent;
             this.oraCurenta.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.oraCurenta.Location = new System.Drawing.Point(668, 9);
+            this.oraCurenta.Location = new System.Drawing.Point(891, 11);
+            this.oraCurenta.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.oraCurenta.Name = "oraCurenta";
             this.oraCurenta.Size = new System.Drawing.Size(0, 25);
             this.oraCurenta.TabIndex = 2;
@@ -99,40 +102,46 @@
             // 
             // groupBox2
             // 
-            this.groupBox2.Controls.Add(this.pictureBox2);
-            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.pictureBoxGate);
+            this.groupBox2.Controls.Add(this.labelGateState);
             this.groupBox2.Controls.Add(this.label5);
-            this.groupBox2.Location = new System.Drawing.Point(12, 75);
+            this.groupBox2.Location = new System.Drawing.Point(16, 92);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(350, 259);
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox2.Size = new System.Drawing.Size(467, 319);
             this.groupBox2.TabIndex = 5;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Detalii poartă:";
             // 
-            // pictureBox2
+            // pictureBoxGate
             // 
-            this.pictureBox2.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.pictureBox2.Location = new System.Drawing.Point(9, 93);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(175, 98);
-            this.pictureBox2.TabIndex = 11;
-            this.pictureBox2.TabStop = false;
+            this.pictureBoxGate.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBoxGate.Location = new System.Drawing.Point(12, 114);
+            this.pictureBoxGate.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBoxGate.Name = "pictureBoxGate";
+            this.pictureBoxGate.Size = new System.Drawing.Size(447, 187);
+            this.pictureBoxGate.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxGate.TabIndex = 11;
+            this.pictureBoxGate.TabStop = false;
             // 
-            // label6
+            // labelGateState
             // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(90, 50);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(41, 15);
-            this.label6.TabIndex = 10;
-            this.label6.Text = "label6";
+            this.labelGateState.AutoSize = true;
+            this.labelGateState.Location = new System.Drawing.Point(100, 62);
+            this.labelGateState.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelGateState.Name = "labelGateState";
+            this.labelGateState.Size = new System.Drawing.Size(38, 16);
+            this.labelGateState.TabIndex = 10;
+            this.labelGateState.Text = "idle...";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(6, 50);
+            this.label5.Location = new System.Drawing.Point(8, 62);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(77, 15);
+            this.label5.Size = new System.Drawing.Size(84, 16);
             this.label5.TabIndex = 9;
             this.label5.Text = "Stare poartă:";
             // 
@@ -145,9 +154,11 @@
             this.groupBox1.Controls.Add(this.label4);
             this.groupBox1.Controls.Add(this.label3);
             this.groupBox1.Controls.Add(this.label2);
-            this.groupBox1.Location = new System.Drawing.Point(390, 75);
+            this.groupBox1.Location = new System.Drawing.Point(520, 92);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(350, 259);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox1.Size = new System.Drawing.Size(467, 319);
             this.groupBox1.TabIndex = 6;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Detalii angajat:";
@@ -155,63 +166,70 @@
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(51, 164);
+            this.label9.Location = new System.Drawing.Point(68, 202);
+            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(41, 15);
+            this.label9.Size = new System.Drawing.Size(44, 16);
             this.label9.TabIndex = 15;
             this.label9.Text = "label9";
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(105, 124);
+            this.label8.Location = new System.Drawing.Point(140, 153);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(41, 15);
+            this.label8.Size = new System.Drawing.Size(44, 16);
             this.label8.TabIndex = 14;
             this.label8.Text = "label8";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(57, 84);
+            this.label7.Location = new System.Drawing.Point(76, 103);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(41, 15);
+            this.label7.Size = new System.Drawing.Size(44, 16);
             this.label7.TabIndex = 13;
             this.label7.Text = "label7";
             // 
             // pictureBox1
             // 
             this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.pictureBox1.Location = new System.Drawing.Point(225, 84);
+            this.pictureBox1.Location = new System.Drawing.Point(300, 103);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(100, 95);
+            this.pictureBox1.Size = new System.Drawing.Size(133, 117);
             this.pictureBox1.TabIndex = 12;
             this.pictureBox1.TabStop = false;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(6, 164);
+            this.label4.Location = new System.Drawing.Point(8, 202);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(38, 15);
+            this.label4.Size = new System.Drawing.Size(41, 16);
             this.label4.TabIndex = 11;
             this.label4.Text = "Sens:";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(6, 124);
+            this.label3.Location = new System.Drawing.Point(8, 153);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(92, 15);
+            this.label3.Size = new System.Drawing.Size(97, 16);
             this.label3.TabIndex = 10;
             this.label3.Text = "Număr mașină:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(6, 84);
+            this.label2.Location = new System.Drawing.Point(8, 103);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(44, 15);
+            this.label2.Size = new System.Drawing.Size(46, 16);
             this.label2.TabIndex = 9;
             this.label2.Text = "Nume:";
             // 
@@ -220,9 +238,10 @@
             this.btnDenyAcces.BackColor = System.Drawing.Color.Red;
             this.btnDenyAcces.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnDenyAcces.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.btnDenyAcces.Location = new System.Drawing.Point(156, 356);
+            this.btnDenyAcces.Location = new System.Drawing.Point(208, 438);
+            this.btnDenyAcces.Margin = new System.Windows.Forms.Padding(4);
             this.btnDenyAcces.Name = "btnDenyAcces";
-            this.btnDenyAcces.Size = new System.Drawing.Size(125, 43);
+            this.btnDenyAcces.Size = new System.Drawing.Size(167, 53);
             this.btnDenyAcces.TabIndex = 7;
             this.btnDenyAcces.Text = "DENY ACCESS";
             this.btnDenyAcces.UseVisualStyleBackColor = false;
@@ -233,9 +252,10 @@
             this.btnLogs.BackColor = System.Drawing.SystemColors.ActiveBorder;
             this.btnLogs.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnLogs.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.btnLogs.Location = new System.Drawing.Point(12, 356);
+            this.btnLogs.Location = new System.Drawing.Point(16, 438);
+            this.btnLogs.Margin = new System.Windows.Forms.Padding(4);
             this.btnLogs.Name = "btnLogs";
-            this.btnLogs.Size = new System.Drawing.Size(125, 43);
+            this.btnLogs.Size = new System.Drawing.Size(167, 53);
             this.btnLogs.TabIndex = 8;
             this.btnLogs.Text = "Logs";
             this.btnLogs.UseVisualStyleBackColor = false;
@@ -243,19 +263,20 @@
             // 
             // btnDisconnectPortar
             // 
-            this.btnDisconnectPortar.Location = new System.Drawing.Point(670, 399);
+            this.btnDisconnectPortar.Location = new System.Drawing.Point(893, 491);
+            this.btnDisconnectPortar.Margin = new System.Windows.Forms.Padding(4);
             this.btnDisconnectPortar.Name = "btnDisconnectPortar";
-            this.btnDisconnectPortar.Size = new System.Drawing.Size(87, 23);
+            this.btnDisconnectPortar.Size = new System.Drawing.Size(116, 28);
             this.btnDisconnectPortar.TabIndex = 9;
             this.btnDisconnectPortar.Text = "Deconectare";
             this.btnDisconnectPortar.UseVisualStyleBackColor = true;
             // 
             // mainPage
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
-            this.ClientSize = new System.Drawing.Size(769, 434);
+            this.ClientSize = new System.Drawing.Size(1025, 534);
             this.Controls.Add(this.btnDisconnectPortar);
             this.Controls.Add(this.btnLogs);
             this.Controls.Add(this.btnDenyAcces);
@@ -265,6 +286,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pbBackground);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.MinimizeBox = false;
             this.Name = "mainPage";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -274,7 +296,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.pbBackground)).EndInit();
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxGate)).EndInit();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
@@ -297,13 +319,13 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button btnDenyAcces;
         private System.Windows.Forms.Button btnLogs;
-        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label labelGateState;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label9;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Timer timer1;
-        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBoxGate;
         private System.Windows.Forms.Button btnDisconnectPortar;
     }
 }
--- PcMod/proiectIP/Form1.cs
+++ PcMod/proiectIP/Form1.cs
@@ -5,6 +5,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.IO;
+using System.IO.Ports;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,63 @@ namespace proiectIP
 
     public partial class mainPage : Form
     {
+        //private SerialPort _serialPort;
+        private Timer _timer;
+    public mainPage()
+        {
+            InitializeComponent();
+            //InitializeSerialPort();
+            InitializeTimer();
+        }
+
+        /*private void InitializeSerialPort()
+        {
+            _serialPort = new SerialPort("COM3", 9600); // Replace "COM3" with your Arduino's serial port
+            _serialPort.DataReceived += SerialPort_DataReceived;
+            _serialPort.Open();
+        }*/
+
+        private void InitializeTimer()
+        {
+            _timer = new Timer();
+            _timer.Interval = 3000; // 3 seconds interval
+            _timer.Tick += Timer_Tick;
+            _timer.Start();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            string[] states = { "inchis", "deschis", "deschidere" };
+            Random random = new Random();
+            int index = random.Next(states.Length);
+            string state = states[index];
+            UpdateUI(state);
+        }
+
+        /*private void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            string data = _serialPort.ReadLine().Trim();
+            Invoke(new Action(() => UpdateUI(data)));
+        }*/
+
+        private void UpdateUI(string state)
+        {
+            labelGateState.Text = state;
+
+            switch (state)
+            {
+                case "inchis":
+                    pictureBoxGate.Image = Properties.Resources.inchis; // Replace with your closed gate image
+                    break;
+                case "deschis":
+                    pictureBoxGate.Image = Properties.Resources.deschis; // Replace with your opened gate image
+                    break;
+                case "deschidere":
+                    pictureBoxGate.Image = Properties.Resources.deschidere; // Replace with your opening gate image
+                    break;
+            }
+        }
+
         // Add these fields inside the MainForm class
         private BluetoothClient _bluetoothClient;
         private Stream _stream;
@@ -40,10 +98,6 @@ namespace proiectIP
                 await Task.Run(() => _bluetoothClient.Connect(targetDevice.DeviceAddress, serviceClassId));
                 _stream = _bluetoothClient.GetStream();
             }
-        }
-    public mainPage()
-        {
-            InitializeComponent();
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -75,6 +129,12 @@ namespace proiectIP
                 // Add code to save any unsaved data here if needed
                 Application.Exit();
             }
+        }
+        
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            //_serialPort.Close();
+            base.OnFormClosing(e);
         }
     }
 }
